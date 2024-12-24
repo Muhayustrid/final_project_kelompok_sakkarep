aa6fa27fa5c762983875b2a0dd87b049f701ce28
Muhayustrid <l200220140@student.ums.ac.id>
2024-12-24 16:40:02 +0700

    Update api.ts

--- website/restoran/src/lib/api.ts
+++ website/restoran/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Konfigurasi base URL untuk API Django
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://finalprojectkelompoksakkarep-production.up.railway.app/api';
 
 // Tipe data untuk berbagai entitas
 export interface Menu {
